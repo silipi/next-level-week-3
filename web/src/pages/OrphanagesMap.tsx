d5ae96adae5c72dcd8fc7b3953adc1691f12303b
Gabriel Silipi <gabriel.silipi@gmail.com>
2020-10-12 11:40:25 -0300

    primeiro dia finalizado

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Map, TileLayer } from 'react-leaflet';
+
+import { FiPlus } from 'react-icons/fi';
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
+import 'leaflet/dist/leaflet.css';
+
+export default function OrphanagesMap() {
+  return (
+    <div>
+      <div id="page-map">
+
+        <aside>
+          <header>
+            <img src={mapMarkerImg} alt="Happy"/>
+
+            <h2>Escolha um orfanato no mapa</h2>
+            <p>Muitas crianças estão esperando a sua visita :</p>
+          </header>
+
+          <footer>
+            <strong>Cascavel</strong>
+            <span>Paraná</span>
+          </footer>
+        </aside>
+
+        <Map 
+          center={[-24.9841483,-53.423017]}
+          zoom={15}
+          style={{width: '100%', height: '100%'}}
+        >
+          <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+        </Map>
+
+        <Link to="" className="create-orphanage">
+          <FiPlus size={32} color="#FFF" />
+        </Link>
+      </div>
+    </div>
+  )
+}
